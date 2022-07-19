9bba707cc115d0046caa0b1822175eded96e9e24
DenDG23 <dendg234@yandex.ru>
2022-07-19 16:00:52 +0300

    added some parameter, styled better

--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
+from datetime import date
+from functools import update_wrapper
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,29 +8,27 @@ from django.shortcuts import render
 import json  # import json to load json data to python dictionary
 import urllib.request  # urllib.request to make a request to api
 
-# 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={&appid=ac3d4963f935c2146c221ebc2f8ec8c4}'
-
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
 
-        ''' api key might be expired use your own api_key
-            place api_key in place of appid ="your_api_key_here "  '''
         # source contain JSON data from API
         source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=ac3d4963f935c2146c221ebc2f8ec8c4&units=metric').read()
 
         # converting JSON data to a dictionary
         list_of_data = json.loads(source)
 
+        mmRtSt = int(list_of_data['main']['pressure']) * 3 / 4
+
         # data for variable list_of_data
         data = {
-            'city': city,
+            'city': city.capitalize(),
             "country_code": str(list_of_data['sys']['country']),
-            "coordinate": str(list_of_data['coord']['lon']) + ' '
-                        + str(list_of_data['coord']['lat']),
-            "temp": str(list_of_data['main']['temp']) + ' c',
-
-            "humidity": str(list_of_data['main']['humidity']) + '%',
+            "temp": str(list_of_data['main']['temp']) + ' ℃',
+            "pressure": str(mmRtSt) + ' mmhg',
+            "visibility": str(list_of_data['visibility']) + ' meters',
+            "wind_speed":str (list_of_data['wind']['speed']) + ' m/s',
+            "clouds": str(list_of_data['clouds']['all']) + '%',
         }
         print(data)
     else:
